9f791685b3fb29e8d6f6673efaee120db00252e9
brandonTsai <brandonya125@gmail.com>
2021-03-19 21:01:16 -0700

    Adding city name display feature

--- clock.js
+++ clock.js
@@ -1,4 +1,10 @@
+function getCity(){
+  const optionIndex = document.getElementById('time-zone').options.selectedIndex
+  const cityName = document.getElementById('time-zone').options[optionIndex].label
+  document.getElementById('city').innerHTML = cityName
+}
+
 setInterval(function(){
   const timeZone = document.getElementById('time-zone').value
-  document.getElementById("clock").innerHTML = new Date().toLocaleString('en-US', { timeZone: timeZone})
+  document.getElementById("clock").innerHTML = new Date().toLocaleString('en-US', { timeZone: timeZone })
 }, 1000);
